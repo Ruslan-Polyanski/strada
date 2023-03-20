c10a11e33f729eedbcfd6741e23c0e0ecc76436c
Ruslan Polyanski <ruslapolyanski@yandex.by>
2023-03-20 20:03:34 +0300

    feat: add new code

--- main.js
+++ main.js
@@ -1,13 +1,6 @@
 
-const COLOR_RED = '#f00';
-const COLOR_GREEN = "#0F0";
+const arr = [1, 2, 3, 4, 5];
 
-let color = COLOR_GREEN;
+console.log(arr.at(-1))
 
-console.log(color);
-
-const myAgeIn2023 = 2023 - 1984;
-console.log(myAgeIn2023);
-
-console.log(1 + 2 + 3 + 4 + 5);
 
