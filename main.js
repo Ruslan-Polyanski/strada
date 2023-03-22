31a869735669e2fa6c9f51fc17d12d641ad76ab1
Ruslan Polyanski <ruslapolyanski@yandex.by>
2023-03-22 19:04:18 +0300

    feat: add new move

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 
-const arr = [1, 2, 3, 4, 5];
 
-console.log(arr.at(-1))
+let login = prompt('Login', '');
+let password;
+
+if( login === 'Админ'){
+    password = prompt('Password', '');
+    if(password === 'Я главный'){
+        alert('Здравствуйте')
+    } else {
+        alert('Неверный пароль')
+    }
+} else if(login === ''){
+    alert('Отменено');
+} else {
+    alert('I dont know you');
+}
+
+
 
 
