c187c73da53916aab9815b7829e8ed0262224966
Ruslan Polyanski <ruslapolyanski@yandex.by>
2023-03-25 12:30:59 +0300

    feat: add switch

--- main.js
+++ main.js
@@ -1,31 +1,13 @@
 
-
-function checkAge(age){
-    if(age >= 18){
-        return "You are welcom";
-    } else {
-        return "You are not allowed";
-    }
-}
-
-let message = checkAge(19);
-
-console.log(message)
-
-
 function calc(a, b, operation){
-    if(operation === 'add'){
-        return a + b;
-    }
-    if(operation === 'multi'){
-        return a * b;
-    }
-    if(operation === 'subtract'){
-        return a - b;
-    }  
+    switch (operation){
+        case ('add'): return a + b;
+        case ('multi'): return a * b;
+        case ('subtract'): return a - b;
+    } 
 }
 
-let result = calc(5, 1, "multi");
+let result = calc(5, 1, "subtract");
 
 console.log(result)
 
